787349a95de3b6b3c414ab5dcbad9c11324433b8
mickami742 <mickami742@gmail.com>
2023-11-06 19:48:12 +0100

    Version 1.0

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    class Weather
+    {
+        public string adress {  get; set; }
+        public DateTime date {  get; set; }
+        public int tempMax {  get; set; }
+        public int tempMin { get; set; }
+    }
+}
